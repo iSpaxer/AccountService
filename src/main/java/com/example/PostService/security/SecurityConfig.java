acea198173fd8487c614903b1875bf20bdd9e1e6
iSpaxer <a.v.sybahin@mail.ru>
2025-06-17 14:24:05 +0300

    Basic аутификация

--- src/main/java/com/example/PostService/security/SecurityConfig.java
+++ src/main/java/com/example/PostService/security/SecurityConfig.java
@@ -2,28 +2,85 @@ package com.example.PostService.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
 
 @Configuration
 public class SecurityConfig {
+
+    private static String VESION_API(String s) {
+        var version = "v1";
+        return "/api/" + version + s;
+    }
 
 //    @Bean
 //    public PasswordEncoder passwordEncoder() {
 //        return new BCryptPasswordEncoder();
 //    }
 
-//    @Bean
-//    public SecurityFilterChain chain(HttpSecurity http) {
-//        http
-//    }
+    @Bean
+    @Order(1)
+    public SecurityFilterChain chainAPI(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .securityMatcher("/api/**")
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.GET, VESION_API("/user/{id:[1-9]\\d*}/**")).permitAll()
+                        .requestMatchers(HttpMethod.GET, VESION_API("{id:[1-9]\\d*}/post")).permitAll()
+                        .requestMatchers(VESION_API("login"), VESION_API("/create")).anonymous()
+                        .requestMatchers(VESION_API("{id:[1-9]\\d*}/**")).authenticated()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(AbstractHttpConfigurer::disable);
+
+        return http.build();
+    }
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        InMemoryUserDetailsManager manager = new InMemoryUserDetailsManager();
-        manager.createUser(User.withDefaultPasswordEncoder().username("user").password("password").roles("USER").build());
-        return manager;
+    @Order(2)
+    public SecurityFilterChain chainAdmin(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .securityMatcher(VESION_API("/admin/**"))
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(VESION_API("/admin/**")).hasRole("ADMIN")
+                )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+
+    @Bean
+    @Order(3)
+    public SecurityFilterChain chainDefault(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers("/**").permitAll()
+                )
+                .httpBasic(Customizer.withDefaults())
+                .formLogin(AbstractHttpConfigurer::disable);
+        return http.build();
+    }
+
+
+    @Bean
+    public UserDetailsService users() {
+        UserDetails user = User.withDefaultPasswordEncoder()
+                .username("user")
+                .password("password")
+                .roles("USER")
+                .build();
+        return new InMemoryUserDetailsManager(user);
     }
 
 }
